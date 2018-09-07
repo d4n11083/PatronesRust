814940aecf169248af38ed9118ec41fd0a0e5c4b
Josué Chaves Araya <d4n11083@estudiantec.cr>
2018-09-07 15:21:27 -0600

    Actualizacion del builder

--- builder.rs
+++ builder.rs
@@ -1,42 +1,58 @@
+
 use std::fmt;
 
 #[derive(Debug)]
-enum Language {
-    English,
-    German,
+enum Lenguaje{
+    Ingles,
+    Espanol,
 }
 
 #[derive(Debug)]
-struct Greeter {
-    language: Language,
+struct Greeter{
+ lenguaje : Lenguaje,
 }
 
-impl fmt::Display for Greeter {
-    fn fmt(&self, f: &mut fmt::Formatter) -> fmt::Result {
-        let greeting = match self.language {
-            Language::English => "Hello",
-            Language::German => "Hallo",
+/*
+ *Se Crea un display para greeter
+ */
+impl fmt::Display for Greeter{
+    fn fmt(&self, f: &mut fmt::Formatter) -> fmt::Result{
+        let saludo = match self.lenguaje{
+            Lenguaje::Ingles => "Hello",
+            Lenguaje::Espanol => "Hola",
         };
-        write!(f, "{} Rust", greeting)
+        write!(f, "{} Rust", saludo)
     }
 }
 
-impl Greeter {
-    fn new() -> Greeter {
-        Greeter {
-            language: Language::English,
+/*
+ * Se implementa el Greeter con el idioma inglés como default
+ * pero se puede construir con el lenguaje que se desea, en este caso
+ * inglés o español.
+ */
+impl Greeter{
+    fn new()-> Greeter{
+        Greeter  {
+            lenguaje : Lenguaje::Ingles,
         }
     }
 
-    fn with_language(mut self, language: Language) -> Greeter {
-        self.language = language;
+    fn with_language(mut self, lenguaje : Lenguaje) -> Greeter{
+        self.lenguaje = lenguaje;
         self
     }
 }
 
 
-fn main() {
-        let greeter = Greeter::new().with_language(Language::German);
-        assert_eq!(format!("{}", greeter), "Hallo Rust");
-        fmt();
+fn main(){
+    
+    //Se crea un greeter en español
+    let greeter = Greeter::new().with_language(Lenguaje:: Espanol);
+    assert_eq!(format!("{}", greeter), "Hola Rust");
+    println!("{:?}", greeter);
+
+    //Se crea un greeter en inglés
+    let greeter2 = Greeter::new().with_language(Lenguaje:: Ingles);
+    assert_eq!(format!("{}", greeter2), "Hello Rust");
+    println!("{:?}", greeter2);
 }
